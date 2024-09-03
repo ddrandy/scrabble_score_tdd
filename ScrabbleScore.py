2f170d31bb576c934175d25a5958cae2472db602
Randy <dddrandy@gmail.com>
2024-09-03 17:30:05 +0930

    Add ScrabbleScore class that take input and meet unit test cases.

--- ScrabbleScore.py
+++ ScrabbleScore.py
@@ -21,3 +21,15 @@ class ScoreCalculator(object):
             if char in LETTERS:
                 sum += LETTERS[char]
         return sum
+
+
+class ScrabbleScore:
+    scoreCalc = ScoreCalculator()
+
+    def run(self):
+        word = input("Please enter a word:")
+        if not self.isAlphaOnly(word):
+            print("Please enter alphabet only.")
+        else:
+            score = self.scoreCalc.calc(word)
+            print(f'Score of "{word}" is: {score}')
