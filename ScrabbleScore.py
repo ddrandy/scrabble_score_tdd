3682d810447ee396bbc2ed16d2f5006fac5e7ba2
Randy <dddrandy@gmail.com>
2024-09-03 14:49:11 +0930

    Add Letters dict with given values.

---
+++ ScrabbleScore.py
@@ -0,0 +1,13 @@
+LETTERS = {
+    x: y
+    for key, y in {
+        "AEIOULNRST": 1,
+        "DG": 2,
+        "BCMP": 3,
+        "FHVWY": 4,
+        "K": 5,
+        "JX": 8,
+        "QZ": 10,
+    }.items()
+    for x in key
+}
