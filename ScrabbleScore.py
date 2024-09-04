2ff75c043cb10afe76f0dd62c6de58d8f11b7261
Randy <dddrandy@gmail.com>
2024-09-04 15:08:26 +0930

    Add countdown timer logic to meet unit test cases.

--- ScrabbleScore.py
+++ ScrabbleScore.py
@@ -1,3 +1,7 @@
+import time
+import threading
+from typing import Callable
+
 LETTERS = {
     x: y
     for key, y in {
@@ -28,8 +32,28 @@ class ScrabbleScore:
 
     def run(self):
         word = input("Please enter a word:")
-        if not self.isAlphaOnly(word):
+        if not word.isalpha():
             print("Please enter alphabet only.")
         else:
             score = self.scoreCalc.calc(word)
             print(f'Score of "{word}" is: {score}')
+
+    def countdown(
+        self,
+        timeout,
+        tick=1,
+        tick_call: Callable[[int], None] = None,
+        timeout_call: Callable[[], None] = None,
+    ) -> threading.Thread:
+        def run_countdown():
+            nonlocal timeout
+            while timeout > 0:
+                tick_call(timeout)
+                time.sleep(tick)
+                timeout -= tick
+            timeout_call()
+
+        countdown_thread = threading.Thread(target=run_countdown)
+        countdown_thread.daemon = True
+        countdown_thread.start()
+        return countdown_thread
